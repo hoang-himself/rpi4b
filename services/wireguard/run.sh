647c921dbbda41de79f4a52696ae303a089ed0d7
Hoang NGUYEN <baauco@gmail.com>
2023-07-07 17:40:36 +0000

    refactor: split long commands

--- services/wireguard/run.sh
+++ services/wireguard/run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 podman run -d --replace -p 51820:51820/udp --name wireguard \
-  -e PUID=1000 -e PGID=1000 -e TZ=UTC -e SERVERURL=auto -e SERVERPORT=51820 \
-  -e PEERS=3 -e PEERDNS=auto -e INTERNAL_SUBNET=192.168.0.0 \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=UTC \
+  -e SERVERURL=auto \
+  -e SERVERPORT=51820 \
+  -e PEERS=3 \
+  -e PEERDNS=auto \
+  -e INTERNAL_SUBNET=192.168.0.0 \
   -v '/opt/wireguard-server/config:/config:Z' \
   -v '/lib/modules:/lib/modules:Z' \
   --sysctl net.ipv4.conf.all.src_valid_mark=1 \
