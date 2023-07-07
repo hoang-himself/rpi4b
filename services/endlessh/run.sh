14de486ad4db430fce5b92369fde4715b6b35ed8
Hoang NGUYEN <baauco@gmail.com>
2023-07-07 17:41:31 +0000

    feat: update endlessh configs

--- services/endlessh/run.sh
+++ services/endlessh/run.sh
@@ -3,7 +3,7 @@
 podman run -d --replace -p 22:2222 --name endlessh \
   -e PUID=1000 \
   -e PGID=1000 \
-  -e TZ=UTC \
+  -e TZ=Etc/UTC \
   -e MSDELAY=10000 \
   -e MAXLINES=32 \
   -e MAXCLIENTS=4096 \
