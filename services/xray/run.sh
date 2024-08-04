ef508a31f24a56b1313ab9187705bc71cd025860
Hoang NGUYEN <baauco@gmail.com>
2024-08-04 15:46:30 +0000

    refactor(services): drop `xray`

--- services/xray/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-podman container run -d --replace -p 80:80 --name xray \
-  -v 'xray:/etc/xray' \
-  -v "$PWD/config.json:/etc/xray/config.json:z" \
-  docker.io/teddysun/xray:latest
